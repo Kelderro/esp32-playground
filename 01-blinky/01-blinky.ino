208f2b8d513db98545ba45443b9752e780bdc5ad
Rob op den Kelder <dev@kelderro.nl>
2021-01-28 21:11:56 +0000

    #1 Finished the first version of the code that will let the LED on the ESP32 blink

--- 01-blinky/01-blinky.ino
+++ 01-blinky/01-blinky.ino
@@ -1,9 +1,23 @@
-void setup() {
-  // put your setup code here, to run once:
+// Arduino pin number of the blue LED
+const int ledPin = A12;
 
+void setup() {
+  // Set the LED pin to OUTPUT as we want to provide a signal to the
+  // pin instead of reading from it (INPUT). If you do not set the mode
+  // the LED may appear dim.
+  pinMode(ledPin, OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  // Set the voltage of the pin to 5V
+  digitalWrite(ledPin, HIGH);
 
+  // Wait for 50 miliseconds to notice for a human that the LED is on
+  delay(50);
+  
+  // Set the voltage of the pin to 0v
+  digitalWrite(ledPin, LOW);
+
+  // Wait for 2 seconds before continue
+  delay(2000);
 }
